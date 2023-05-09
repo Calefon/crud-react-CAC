a29b0bbe05b897a2c2a6f3a4c82796519356e431
Ezequiel Gonzalez <eze5025@gmail.com>
2023-05-08 23:47:25 -0300

    update 2

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -1,11 +1,55 @@
 import{useState,useEffect} from "react";
 import {Link} from "react-router-dom";
 import{collection,getDocs,deleteDoc,doc} from "firebase/firestore";
-import {db} from "../fireBaseConfig/firebase.js";
+import {db} from "../firebaseConfig/firebase.js";
 
-import Swal from "sweetalert2";
+import Swal from "sweetalert2"; 
 import withReactContent from "sweetalert2-react-content";
 const mySwal = withReactContent(Swal);
+
+const DeleteButton = ({onClick}) => { return (
+    <button onClick={onClick} className="btn btn-danger">
+        <i className="fa-solid fa-trash"></i>
+    </button>
+    )};
+
+const EditButton = ({productId}) => {
+    return(
+    <Link to={`/edit/${productId}`} className="btn btn-light">
+        <i className="fa-solid fa-pencil"></i>
+    </Link>
+    );}
+
+const AwesomeProductsTable = ({productsArray,deleteFunction}) => {
+    return (
+        <table className="table table-dark table-hover">
+            <thead>
+                <tr>
+                <th>Title</th>
+                <th>Authors</th>
+                <th>Stock</th>
+                <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {productsArray.map(
+                    book => {
+                        return(
+                        <tr key={book.id}>
+                            <td>{book.title}</td>
+                            <td>{book.authors}</td>
+                            <td>{book.stock}</td>
+                            <td>
+                                <EditButton productId={book.id}/>
+                                <DeleteButton onClick={()=>deleteFunction(book.id)}/>
+                            </td>
+                        </tr>);
+                    }
+                )}
+            </tbody>
+        </table>
+    );
+}
 
 const Show = ()=>{
     //configuramos useState
@@ -15,16 +59,16 @@ const Show = ()=>{
 
     const getBooks = async()=>{
         const data = await getDocs(booksCollection);
-        console.log(data.docs)
+        
         setBooks(
             data.docs.map((doc)=>({...doc.data(),id:doc.id}))
         );
-        console.log(data.docs.map((doc)=>({...doc.data(),id:doc.id})))
     }
 
     const deleteBook = async(id)=>{
         const bookDoc = doc(db,"products",id);
         await deleteDoc(bookDoc);
+        getBooks();
     }
 
     const confirmDelete = (id)=>{
@@ -54,34 +98,17 @@ const Show = ()=>{
 
     return(
         <>
-        <h1>Products</h1>
-        <table>
-            <thead>
-                <tr>
-                <th>Title</th>
-                <th>Authors</th>
-                <th>Stock</th>
-                <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {books.map(
-                    book => {
-                        return(
-                        <tr key={book.id}>
-                            <td>{book.title}</td>
-                            <td>{book.authors}</td>
-                            <td>{book.stock}</td>
-                            <td>
-                                <Link to={"/create"}></Link>
-                                
-                            </td>
-                        </tr>);
-                    }
-                )}
-            </tbody>
-        </table>
-</>
+        <div className="container">
+            <div className="col">
+                <div className="row">
+                    <div className="d-grid gap-2">
+                        <Link to="/create" className="btn btn-secondary mt-2 mb-2">CREAR</Link>
+                    </div>
+                    <AwesomeProductsTable productsArray={books} deleteFunction={confirmDelete}/>
+                </div>
+            </div>
+        </div>
+        </>
     );
 }
 
